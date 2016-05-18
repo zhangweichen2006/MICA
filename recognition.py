d50895bd05156de1ab242aeba68c9a7b156dd034
zhangweichen2006 <weichenzhang@vlan-2620-10-16-86-66.student.wireless.sydney.edu.au>
2016-05-18 10:33:57 +1000

    New dejavu

--- recognition.py
+++ recognition.py
@@ -16,11 +16,13 @@ from dejavu.recognize import FileRecognizer
 
 class Recognition(object):
     def dejavu(self):
-	    config = {"database": {"host": "127.0.0.1", "user": "root", "passwd": "", "db": "dejavu"}}
-	    # config = {"database": {"host": "10.66.31.157", "user": "root", "passwd": "662813", "db": "dejavu"}}
-	    djv = Dejavu(config)
-	    song = djv.recognize(FileRecognizer, "record_music/convchou.mp3")
-	    print(song)
+        # config = {"database": {"host": "127.0.0.1", "user": "root", "passwd": "", "db": "dejavu"}}
+        config = {"database": {"host": "10.66.31.157", "user": "root", "passwd": "662813", "db": "dejavu"}}
+        djv = Dejavu(config)
+        djv.fingerprint_directory("mp3/Piano/", [".mp3"])
+        djv.fingerprint_directory("mp3/Violin/", [".mp3"])
+        song = djv.recognize(FileRecognizer, "output.wav")
+        print(song)
 
 if __name__ == "__main__":
     g = Recognition()
