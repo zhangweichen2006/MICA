39b27c3432d9be5bb0c872cc56e6aa79c3a14c6c
Jiaxi Li <jiaxili0319@gmail.com>
2016-05-20 15:45:24 +1000

    remove unnecessaries

--- example/pathgetter.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-#!/usr/bin/env python
-from sys import path
-import os.path
-thisrep = os.path.dirname(os.path.abspath(__file__))
-path.append(os.path.dirname(thisrep))
-
-from EasyGame import pathgetter
-source = pathgetter()
